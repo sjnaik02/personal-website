ff10913c309ff4b563774468ca5fa3ec91390d38
Shourya Naik <sjnaik02@gmail.com>
2023-07-26 10:37:03 +0530

    added real projects to the projects section

---
+++ src/lib/projects.js
@@ -0,0 +1,24 @@
+const projects = [
+  {
+    title: "TEDxBITSGoa Website",
+    description:
+      "The official website for TEDxBITSGoa, India's oldest institutional TEDx event. Using NextJS, Tailwind, and Framer Motion.",
+    link: "https://tedxbitsgoa.in",
+    github: "https://github.com/sjnaik02/new-tedx-bitsgoa-website",
+  },
+  {
+    title: "personal-website",
+    description: "My personal website and blog built with Astro and Tailwind.",
+    link: "https://shouryanaik.com",
+    github: "https://github.com/sjnaik02/personal-website",
+  },
+  {
+    title: "Tilde",
+    description:
+      "A simple, minimalistic and fast markdown note taking platform. Built with NextJS, Tailwind, and next-auth.",
+    link: "https://tilde.shouryanaik.com",
+    github: "https://github.com/sjnaik02/tilde",
+  },
+];
+
+export default projects;
