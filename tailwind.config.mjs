2519b21f52b3f6e0e9411344c973832d1ccd68fa
Shourya Naik <sjnaik02@gmail.com>
2024-12-26 22:19:33 +0530

    improving the visuals

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,13 @@ export default {
     extend: {
       fontFamily: {
         serif: ["Instrument Serif", ...defaultTheme.fontFamily.serif],
-        sans: ["Instrument Sans", ...defaultTheme.fontFamily.sans],
+        sans: ["Instrument Sans Variable", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        grid: "radial-gradient(ellipse 100% 80% at 50% 0%, rgba(255,165,0,0.3), transparent), radial-gradient(circle at center, rgba(255,255,255,0) 0%, white 100%), linear-gradient(to right, rgba(0,0,0,0.1) 1px, transparent 1px), linear-gradient(to bottom, rgba(0,0,0,0.1) 1px, transparent 1px)",
+      },
+      backgroundSize: {
+        grid: "100% 100%, 100% 100%, 40px 40px, 40px 40px",
       },
     },
   },
